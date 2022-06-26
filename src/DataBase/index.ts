1718685d7f6fdba42f3a7cc1e09d0b146bd79be0
Victor Braga <victorbraga.8@hotmail.com>
2022-06-26 15:28:30 -0300

    Commit Inicial

--- src/DataBase/index.ts
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://victorbraga:victorbragagh@cluster0.mpucf.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
-
-module.exports = mongoose;
-
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import express from 'express'
-import { router } from './routes';
-
+import express from "express";
 const app = express();
-app.use(express.json());
-app.use(router);
 
-app.listen(app.listen(process.env.PORT || 3333));
+app.listen('3333',()=>{
+  console.log("Servidor Online Atualizado");
+})
